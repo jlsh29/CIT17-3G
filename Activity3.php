8bb0988c8a3368297e78955473f6970d7c73bd0d
S312User <S312User@S312.uc-bcf.edu.ph>
2023-10-09 18:01:12 +0800

    3rdActivity

---
+++ Activity3.php
@@ -0,0 +1,13 @@
+<?php
+$x = 4;
+function assignx () {
+PHP
+33
+$x = 0;
+print "\$x inside function is $x. 
+";
+}
+assignx();
+print "\$x outside of function is $x. 
+";
+?>
---
+++ Activity4.php
@@ -0,0 +1,9 @@
+<?php
+$somevar = 15;
+function addit() {
+GLOBAL $somevar;
+$somevar++;
+print "Somevar is $somevar";
+}
+addit();
+?>
