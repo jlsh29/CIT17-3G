5346de9cabb3b1a44dae7083abb71d882b80e670
Jelisha Mae P. Banaria <jelbanaria@gmail.com>
2023-10-11 03:22:14 +0800

    Updated

--- Intro.php
+++ Intro.php
@@ -11,23 +11,21 @@
     ?>
 
     <p> This is a <?php echo "Jelisha"; ?> paragraph </p>
-    <?php echo "This is also a paragraph"; ?>
+    <?php echo "This is also a paragraph <br>"; ?>
 
     <?php 
     // Scalar Types (contain one variable)
     $string = "Jelisha Mae";
     $int = 23;
-    $float = 23.23333
-    //$boolean = true;
+    $float = 23.23333;
+    $bool = true;
 
     //Array type (Multiple variable)
     $name = array("Jelisha" , "Renz");
     $array = ["Jelisha" , "Renz"];
 
-    echo $string;
-    echo $int; 
-    echo $float; 
-
+    echo "$string <br> $int <br> $float"; 
+    
     ?>
 </body>
 </html>
