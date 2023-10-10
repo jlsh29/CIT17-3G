dd2fdbc680c834d6e3570066d6f03930a5f11962
Jelisha Mae P. Banaria <jelbanaria@gmail.com>
2023-10-11 03:09:34 +0800

    Introduction PHP

---
+++ Intro.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // single comment
+    /* multiple comment */
+    // Syntax only for PHP.
+    ?>
+
+    <p> This is a <?php echo "Jelisha"; ?> paragraph </p>
+    <?php echo "This is also a paragraph"; ?>
+
+    <?php 
+    // Scalar Types (contain one variable)
+    $string = "Jelisha Mae";
+    $int = 23;
+    $float = 23.23333
+    //$boolean = true;
+
+    //Array type (Multiple variable)
+    $name = array("Jelisha" , "Renz");
+    $array = ["Jelisha" , "Renz"];
+
+    echo $string;
+    echo $int; 
+    echo $float; 
+
+    ?>
+</body>
+</html>
