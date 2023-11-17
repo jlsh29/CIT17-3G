b1617bdc2a4cdf2c37509eea7011ed29a33df46f
Jelisha Mae P. Banaria <jelbanaria@gmail.com>
2023-11-17 15:49:12 +0800

    New

---
+++ MySQL.php
@@ -0,0 +1,32 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "StudentRecord";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+die("Connection Failed: " . $conn->connect_error);
+} else {
+echo "Connected Succesfully" . "<br>" ;
+}
+
+
+
+$sql = "SELECT * FROM student";
+$result = $conn->query("$sql");
+
+    if ($result) {
+        while ($row = $result->fetch_assoc()) {
+            echo    "Student ID: " . $row["studentID"] . "<br>" .
+                    "First Name: " . $row["firstName"] . "<br>" .
+                    "Last Name: " . $row["lastName"] . "<br>";
+            }
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->connect_error;
+    }
+    $conn->close();
+?>
